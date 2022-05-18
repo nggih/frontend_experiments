282efcadaed5df9fbae849cf026cfc1275b21d9d
nggih <linggih.saputro@sci.ui.ac.id>
2022-05-18 10:47:29 +0700

    add filtered Events

--- nextjs_scratch/components/events/events-search.js
+++ nextjs_scratch/components/events/events-search.js
@@ -1,17 +1,31 @@
+import Button from "../ui/button";
+import classes from "./events-search.module.css";
+import {useRef} from "react";
+
 function EventsSearch(props) {
+  const yearInputRef = useRef();
+  const monthInputRef = useRef();
+
+  function submitHandler(event) {
+    // event.preventDefault(); to prevent the browser default off sendign an http request which will lose applications state when reload our page .
+    event.preventDefault(); 
+    const selectedYear = yearInputRef.current.value;
+    const selectedMonth = monthInputRef.current.value;
+    props.onSearch(selectedYear, selectedMonth)
+  }
   return (
-    <form>
-      <div>
-        <div>
+    <form className={classes.form} onSubmit={submitHandler}>
+      <div className={classes.controls}>
+        <div className={classes.control}>
           <label htmlFor="year">Year</label>
-          <select id="year">
+          <select id="year" ref={yearInputRef}>
             <option value="2021">2021</option>
             <option value="2022">2022</option>
           </select>
         </div>
-        <div>
+        <div className={classes.control}>
           <label htmlFor="month">Month</label>
-          <select id="month">
+          <select id="month" ref={monthInputRef}>
             <option value="1">January</option>
             <option value="2">Feb</option>
             <option value="3">Mar</option>
@@ -27,6 +41,7 @@ function EventsSearch(props) {
           </select>
         </div>
       </div>
+      <Button>Find Events</Button>
     </form>
   );
 }
--- nextjs_scratch/pages/events/index.js
+++ nextjs_scratch/pages/events/index.js
@@ -1,11 +1,23 @@
 import { getAllEvents } from "../../dummy-data";
 import EventList from "../../components/events/event-list";
+import EventsSearch from "../../components/events/events-search";
+import { Fragment } from "react";
+import { Router, useRouter } from "next/router";
 
 function AllEventsPage() {
+  const router = useRouter();
   const events = getAllEvents();
+
+  function findEventsHandler(year, month) {
+    const fullPath = `/events/${year}/${month}`;
+    router.push(fullPath)
+  }
   return (
     <div>
-      <EventList items={events} />
+      <Fragment>
+        <EventsSearch onSearch={findEventsHandler}/>
+        <EventList items={events} />
+      </Fragment>
     </div>
   );
 }
